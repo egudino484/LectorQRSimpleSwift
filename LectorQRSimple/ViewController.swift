c955461f75295c74428a6e6c4f3e104961e41b37
Edison Gudino <egudino@montran.com>
2020-06-22 20:33:52 -0500

    Initial Commit

---
+++ LectorQRSimple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LectorQRSimple
+//
+//  Created by Edison Gudiño on 6/22/20.
+//  Copyright © 2020 Edison Gudiño. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LectorQRSimpleTests/LectorQRSimpleTests.swift
@@ -0,0 +1,34 @@
+//
+//  LectorQRSimpleTests.swift
+//  LectorQRSimpleTests
+//
+//  Created by Edison Gudiño on 6/22/20.
+//  Copyright © 2020 Edison Gudiño. All rights reserved.
+//
+
+import XCTest
+@testable import LectorQRSimple
+
+class LectorQRSimpleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
